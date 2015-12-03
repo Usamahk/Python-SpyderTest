a341631d59f74fe7bba4b8b7b9b72640d818b530
Usamahk <usamah.khan@gmail.com>
2015-12-02 19:32:31 -0500

    Added data folder and started challenger analysis

---
+++ PyMC-Challenger.py
@@ -0,0 +1,9 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Wed Dec  2 19:29:51 2015
+
+@author: Usamahk
+"""
+
+# This file examines the challenger data to determine and model where failure
+# of an O ring will occur due to temperature.
