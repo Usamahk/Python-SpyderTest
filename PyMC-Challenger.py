bfd1d33502498ff6cbab5b9284e7dffd2c0feb7d
Usamahk <usamah.khan@gmail.com>
2015-12-05 00:21:15 -0500

    Finished working chapter 2, need to install seperation_plot

--- PyMC-Challenger.py
+++ PyMC-Challenger.py
@@ -161,3 +161,99 @@ plt.subplot(212)
 plt.hist(alpha_samples, histtype='stepfilled', bins=35, alpha=0.85,
          label=r"posterior of $\alpha$", color="#A60628", normed=True)
 plt.legend();
+
+# Find the expected value of the posterior
+
+t = np.linspace(temperature.min() - 5, temperature.max() + 5, 50)[:, None]
+p_t = logistic(t.T, beta_samples, alpha_samples)
+
+mean_prob_t = p_t.mean(axis=0)
+
+figsize(12.5, 4)
+
+plt.plot(t, mean_prob_t, lw=3, label="average posterior \nprobability \
+of defect")
+plt.plot(t, p_t[0, :], ls="--", label="realization from posterior")
+plt.plot(t, p_t[-2, :], ls="--", label="realization from posterior")
+plt.scatter(temperature, D, color="k", s=50, alpha=0.5)
+plt.title("Posterior expected value of probability of defect; \
+plus realizations")
+plt.legend(loc="lower left")
+plt.ylim(-0.1, 1.1)
+plt.xlim(t.min(), t.max())
+plt.ylabel("probability")
+plt.xlabel("temperature");
+
+# Check with a 95% Confidence interval a range
+
+from scipy.stats.mstats import mquantiles
+
+# vectorized bottom and top 2.5% quantiles for "confidence interval"
+qs = mquantiles(p_t, [0.025, 0.975], axis=0)
+plt.fill_between(t[:, 0], *qs, alpha=0.7,
+                 color="#7A68A6")
+
+plt.plot(t[:, 0], qs[0], label="95% CI", color="#7A68A6", alpha=0.7)
+
+plt.plot(t, mean_prob_t, lw=1, ls="--", color="k",
+         label="average posterior \nprobability of defect")
+
+plt.xlim(t.min(), t.max())
+plt.ylim(-0.02, 1.02)
+plt.legend(loc="lower left")
+plt.scatter(temperature, D, color="k", s=50, alpha=0.5)
+plt.xlabel("temp, $t$")
+
+plt.ylabel("probability estimate")
+plt.title("Posterior probability estimates given temp. $t$");
+
+# Therefore coming back to the challenger data, on the day it was 31 degrees F
+# which makes it seem almost certain that a defect was present.
+
+figsize(12.5, 2.5)
+
+prob_31 = logistic(31, beta_samples, alpha_samples)
+
+plt.xlim(0.995, 1)
+plt.hist(prob_31, bins=1000, normed=True, histtype='stepfilled')
+plt.title("Posterior distribution of probability of defect, given $t = 31$")
+plt.xlabel("probability of defect occurring in O-ring");
+
+# Is the model good? Need to check for a goodness of fit. To do so, simulate
+# data and apply the function we achieve to it
+
+simulated = pm.Bernoulli("bernoulli_sim", p)
+N = 10000
+
+mcmc = pm.MCMC([simulated, alpha, beta, observed])
+mcmc.sample(N)
+
+figsize(12.5, 5)
+
+simulations = mcmc.trace("bernoulli_sim")[:]
+print( simulations.shape)
+
+plt.title("Simulated dataset using posterior parameters")
+figsize(12.5, 6)
+for i in range(4):
+    ax = plt.subplot(4, 1, i + 1)
+    plt.scatter(temperature, simulations[1000 * i, :], color="k",
+                s=50, alpha=0.6)
+                
+# Use data-viz to determine goodness of fit. Plots are what are known as
+# Seperation plots
+
+posterior_probability = simulations.mean(axis=0)
+print("posterior prob of defect | realized defect ")
+for i in range(len(D)):
+    print("%.2f                     |   %d" % (posterior_probability[i], D[i]))
+    
+ix = np.argsort(posterior_probability)
+print("probb | defect ")
+for i in range(len(D)):
+    print("%.2f  |   %d" % (posterior_probability[ix[i]], D[ix[i]]))
+    
+from separation_plot import separation_plot
+
+figsize(11., 1.5)
+separation_plot(posterior_probability, D)
