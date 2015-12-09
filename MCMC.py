d3b855de8b34ef5c4df43665d59b44c527ed58f7
Usamahk <usamah.khan@gmail.com>
2015-12-08 23:23:00 -0500

    Worked chapter 3

--- MCMC.py
+++ MCMC.py
@@ -37,3 +37,26 @@ ax = fig.add_subplot(122, projection='3d')
 ax.plot_surface(X, Y, M, cmap=plt.cm.jet, vmax=1, vmin=-.15)
 ax.view_init(azim=390)
 plt.title("Uniform prior landscape; alternate view");
+
+# We can see a flat surface of all prior distributions. However, if the 
+# distributions for the 2 priors are Exp(3) and Exp(10)
+
+figsize(12.5, 5)
+fig = plt.figure()
+plt.subplot(121)
+
+exp_x = stats.expon.pdf(x, scale=3)
+exp_y = stats.expon.pdf(x, scale=10)
+M = np.dot(exp_x[:, None], exp_y[None, :])
+CS = plt.contour(X, Y, M)
+im = plt.imshow(M, interpolation='none', origin='lower',
+                cmap=jet, extent=(0, 5, 0, 5))
+#plt.xlabel("prior on $p_1$")
+#plt.ylabel("prior on $p_2$")
+plt.title("$Exp(3), Exp(10)$ prior landscape")
+
+ax = fig.add_subplot(122, projection='3d')
+ax.plot_surface(X, Y, M, cmap=jet)
+ax.view_init(azim=390)
+plt.title("$Exp(3), Exp(10)$ prior landscape; \nalternate view")
+
